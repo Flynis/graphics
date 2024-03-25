68cdc8fe72a0850ce4def1836f6cbbd16f769341
ilya <dyakun.ilya@mail.ru>
2024-03-25 13:46:31 +0700

    Chromatic aberration fix

--- picfilter/src/main/java/ru/dyakun/picfilter/transformations/ChromaticAberration.java
+++ picfilter/src/main/java/ru/dyakun/picfilter/transformations/ChromaticAberration.java
@@ -9,14 +9,15 @@ import java.awt.image.BufferedImage;
 public class ChromaticAberration implements ImageTransformation {
 
     public final IntegerProperty red = new IntegerProperty(5, -128, 128, "Red");
-    public final IntegerProperty green = new IntegerProperty(4, -128, 128, "Green");
-    public final IntegerProperty blue = new IntegerProperty(2, -128, 128, "Blue");
+    public final IntegerProperty green = new IntegerProperty(0, -128, 128, "Green");
+    public final IntegerProperty blue = new IntegerProperty(-3, -128, 128, "Blue");
 
-    private int cutOutOfRange(int idx, int w) {
-        if(idx < 0) {
-            return 0;
+    private int cutOutOfRange(int x, int w, int offset) {
+        int idx = x + offset;
+        if(idx < 0 || idx > w - 1) {
+            return x;
         }
-        return Math.min(idx, w - 1);
+        return idx;
     }
 
     @Override
@@ -27,12 +28,9 @@ public class ChromaticAberration implements ImageTransformation {
         int alpha = 0xFF;
         for(int y = 0; y < h; y++) {
             for(int x = 0; x < w; x++) {
-                int idx = x + red.getVal();
-                int rc = ((src.getRGB(cutOutOfRange(idx, w), y) >> 16) & 0xFF);
-                idx = x + green.getVal();
-                int gc = ((src.getRGB(cutOutOfRange(idx, w), y) >> 8) & 0xFF);
-                idx = x + blue.getVal();
-                int bc = (src.getRGB(cutOutOfRange(idx, w), y) & 0xFF);
+                int rc = ((src.getRGB(cutOutOfRange(x, w, red.getVal()), y) >> 16) & 0xFF);
+                int gc = ((src.getRGB(cutOutOfRange(x, w, green.getVal()), y) >> 8) & 0xFF);
+                int bc = (src.getRGB(cutOutOfRange(x, w, blue.getVal()), y) & 0xFF);
                 int c = (alpha << 24) + (rc << 16) + (gc << 8) + bc;
                 dst.setRGB(x, y, c);
             }
